48545747baa514426ce94fdf5349539fe33a0f37
ams177 <ams177@humbodt.edu>
2013-11-06 15:30:30 -0800

    Added Icons
    extensive update to the xml for create new subject page, added icons

--- gen/com/example/studytimer/R.java
+++ gen/com/example/studytimer/R.java
@@ -23,22 +23,48 @@ public final class R {
     public static final class drawable {
         public static final int bar_graph=0x7f020000;
         public static final int bg=0x7f020001;
-        public static final int edit_subjects=0x7f020002;
-        public static final int hit_the_books=0x7f020003;
-        public static final int hit_the_books2=0x7f020004;
-        public static final int ic_launcher=0x7f020005;
-        public static final int view_results=0x7f020006;
+        public static final int button_add_subject=0x7f020002;
+        public static final int edit_subjects=0x7f020003;
+        public static final int header_create_new_subject=0x7f020004;
+        public static final int hit_the_books=0x7f020005;
+        public static final int hit_the_books2=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int icon1=0x7f020008;
+        public static final int icon10=0x7f020009;
+        public static final int icon11=0x7f02000a;
+        public static final int icon12=0x7f02000b;
+        public static final int icon2=0x7f02000c;
+        public static final int icon3=0x7f02000d;
+        public static final int icon4=0x7f02000e;
+        public static final int icon5=0x7f02000f;
+        public static final int icon6=0x7f020010;
+        public static final int icon7=0x7f020011;
+        public static final int icon8=0x7f020012;
+        public static final int icon9=0x7f020013;
+        public static final int icon_box=0x7f020014;
+        public static final int view_results=0x7f020015;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
+        public static final int action_settings=0x7f080013;
         public static final int button1=0x7f080000;
         public static final int button2=0x7f080001;
-        public static final int button3=0x7f080002;
+        public static final int button3=0x7f08000f;
         public static final int button4=0x7f080004;
-        public static final int button5=0x7f080006;
-        public static final int button6=0x7f080007;
-        public static final int chronometer1=0x7f080005;
-        public static final int editText1=0x7f080003;
+        public static final int button5=0x7f080011;
+        public static final int button6=0x7f080012;
+        public static final int chronometer1=0x7f080010;
+        public static final int editText1=0x7f080002;
+        public static final int icon1=0x7f08000a;
+        public static final int icon10=0x7f08000e;
+        public static final int icon11=0x7f08000c;
+        public static final int icon12=0x7f08000d;
+        public static final int icon2=0x7f080009;
+        public static final int icon4=0x7f080008;
+        public static final int icon5=0x7f080007;
+        public static final int icon6=0x7f080006;
+        public static final int icon7=0x7f08000b;
+        public static final int icon8=0x7f080003;
+        public static final int imageView1=0x7f080005;
     }
     public static final class layout {
         public static final int activity_create_new_subject=0x7f030000;
@@ -46,11 +72,8 @@ public final class R {
         public static final int activity_main=0x7f030002;
         public static final int activity_test=0x7f030003;
         public static final int activity_view_results=0x7f030004;
-        public static final int create_subject=0x7f030005;
-        public static final int edit_subject=0x7f030006;
-        public static final int results=0x7f030007;
-        public static final int timer=0x7f030008;
-        public static final int timer_page=0x7f030009;
+        public static final int results=0x7f030005;
+        public static final int timer_page=0x7f030006;
     }
     public static final class menu {
         public static final int create_new_subject=0x7f070000;
